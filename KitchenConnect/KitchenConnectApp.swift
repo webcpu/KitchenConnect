6790e259ec125e2432e5412a54f2c2d83f6b29bc
Liang <liang.yuan@qvik.fi>
2023-04-01 11:28:44 +0200

    Initial Commit

---
+++ KitchenConnect/KitchenConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenConnectApp.swift
+//  KitchenConnect
+//
+//  Created by liang on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
