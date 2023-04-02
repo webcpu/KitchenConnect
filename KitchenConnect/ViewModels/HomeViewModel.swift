55db4ae2fb7e58b5da90747cfbaaf1afbfdba759
Liang <liang.bits@gmail.com>
2023-04-02 22:17:53 +0200

    refactor: Delete unused comments

--- KitchenConnect/ViewModels/HomeViewModel.swift
+++ KitchenConnect/ViewModels/HomeViewModel.swift
@@ -10,10 +10,6 @@ import OrderedCollections
 import Combine
 
 // MARK: - HomeViewModel
-
-/// # KitchenConnect
-///
-/// ## View Models
 ///
 /// A view model for the `HomeView`.
 ///
@@ -69,45 +65,3 @@ class HomeViewModel: ObservableObject {
         }
     }
 }
-
-/*
- class HomeViewModel: ObservableObject {
- 
- // MARK: - Properties
- 
- /// An ordered dictionary of appliances, with the appliance ID as the key.
- @Published var appliances: OrderedDictionary<String, Appliance> = [:]
- 
- /// A list of appliance IDs used for fetching appliances from the remote service.
- let ids = ["12CFD"]
- 
- /// A set of `AnyCancellable` objects for managing Combine subscriptions.
- var bag: Set<AnyCancellable> = []
- 
- // MARK: - Methods
- 
- /// Fetches appliances from the remote service using the appliance IDs.
- ///
- /// This method iterates through the list of appliance IDs, fetching each appliance from the remote
- /// service and updating the appliances dictionary when a response is received.
- func fetchAppliances() {
- for id in ids {
- RemoteService.shared
- .fetchAppliance(id)
- .receive(on: DispatchQueue.main)
- .sink(receiveCompletion: { completion in
- switch completion {
- case .failure(let error):
- print("Error: \(error)")
- case .finished:
- print("Completed")
- }
- }, receiveValue: { appliance in
- self.appliances[appliance.applianceId] = appliance
- print("Appliance: \(appliance)")
- })
- .store(in: &bag)
- }
- }
- }
- */
